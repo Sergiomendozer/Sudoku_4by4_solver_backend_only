f1503d1507e238dba264a0635230c17cf9dd412c
sergiomendozer <sergiomendozansx@gmail.com>
2022-03-02 03:08:08 -0800

    js file set up and html file set up

--- solver.js
+++ solver.js
@@ -1,22 +1,15 @@
 // DOM Manipulation: Selecting & changing website elements
-
+// var unicycle = document.getElementsByClassName("unicycle");
+// var paragraphs = document.getElementsByTagName("p");
+// var queryUnicycle = document.querySelector(".unicycle");
+// var queryAll = document.querySelectorAll(".unicycle, #div2");
 var div1 = document.getElementById("div1");
-
-var unicycle = document.getElementsByClassName("unicycle");
-
-var paragraphs = document.getElementsByTagName("p");
-
-var queryUnicycle = document.querySelector(".unicycle");
-
-var queryAll = document.querySelectorAll(".unicycle, #div2");
-
 var text = "<h1>Hello World</h1>";
 
 // div1.innerHTML = text;
 
 document.getElementById("div1").innerHTML = text;
-// this is here text changes
 
-// for (i = 0; i < queryAll.length; ++i) {
-//   queryAll[i].innerHTML = text;
-// }
+var table = document.getElementById("table");
+var first_sudoku = "<h1>[[1], [2], [3], [4]]</h1>";
+document.getElementById("table").innerHTML = first_sudoku;
