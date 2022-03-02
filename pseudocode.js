eac7d9525d6eaf7f380471f020f2be2162586f37
sergiomendozer <sergiomendozansx@gmail.com>
2022-03-02 03:48:06 -0800

    new fucntion

--- pseudocode.js
+++ pseudocode.js
@@ -6,6 +6,15 @@
 
 //function most instances: order of instances count_1,... which one has the highest number, this is the order in which you put the number in
 
-//! use a class for function N,and Y,
+//! use a class for function N,and Y:
 // goes through position 1,2,3,4... to 16
+// if position has 1-4 number move to next position
+//when at each position it goes from column in row to find number n
+// if row and column do not have number n label as N, change position from X to Y
+// Y this indicates that you can put number Y in position,
+//
+// if row and column does have number n label as N, change position from X to N
+// N this indicates that you can put number -N- in position,
+
+//!put number n in a position
 //
