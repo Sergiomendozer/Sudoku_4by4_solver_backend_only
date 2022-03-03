acebbc834af3cb594024778aaa00291f916f7d9e
sergiomendozer <sergiomendozansx@gmail.com>
2022-03-03 01:11:29 -0800

    full tabel

--- solver.js
+++ solver.js
@@ -7,6 +7,26 @@ var div1 = document.getElementById("div1");
 var text = "<h1>Hello World</h1>";
 document.getElementById("div1").innerHTML = text;
 
-var table = document.getElementById("table");
-var first_sudoku = "<h1>[[X,X,X,X] [1,X,2,X], [X,1,4,X], [2,X,X,1]]</h1>";
-document.getElementById("table").innerHTML = first_sudoku;
+// var first_sudoku = "<h1>[[X,X,X,X] [1,X,2,X], [X,1,4,X], [2,X,X,1]]</h1>";
+// var first_sudoku = "[[X,X,X,X,1,X,2,X,X,1,4,X,2,X,X,1]]";
+// var first_sudoku = [["X","X","X","X"] [1,X,2,X], [X,1,4,X], [2,X,X,1]];
+var table_1 = document.getElementById("table_1");
+var table_2 = document.getElementById("table_2");
+var table_3 = document.getElementById("table_3");
+var table_4 = document.getElementById("table_4");
+var first_sudoku = [
+  ["X", "X", "X", "X"],
+  ["1", "X", "2", "X"],
+  ["X", "1", "4", "X"],
+  ["2", "X", "X", "1"],
+];
+
+var first_col = first_sudoku[0];
+var second_col = first_sudoku[1];
+var third_col = first_sudoku[2];
+var forth_col = first_sudoku[3];
+
+document.getElementById("table_1").innerHTML = first_col;
+document.getElementById("table_2").innerHTML = second_col;
+document.getElementById("table_3").innerHTML = third_col;
+document.getElementById("table_4").innerHTML = forth_col;
