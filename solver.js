c41c2f990695eca733e5492d5e18f016fbf36729
sergiomendozer <sergiomendozansx@gmail.com>
2022-03-03 06:03:55 -0800

    checker for pos0-0

--- solver.js
+++ solver.js
@@ -44,6 +44,7 @@ var count_one = 0;
 var count_two = 0;
 var count_three = 0;
 var count_four = 0;
+var sortable = [];
 
 function counter_1(sudoku, sudoku_num, counter) {
   for (let i = 0; i < 4; i++) {
@@ -52,7 +53,7 @@ function counter_1(sudoku, sudoku_num, counter) {
     }
   }
   count_one = counter;
-  console.log(`one:${counter}`);
+  //   console.log(`one:${counter}`);
 }
 
 function counter_2(sudoku, sudoku_num, counter) {
@@ -62,7 +63,7 @@ function counter_2(sudoku, sudoku_num, counter) {
     }
   }
   count_two = counter;
-  console.log(`two:${counter}`);
+  //   console.log(`two:${counter}`);
 }
 
 function counter_3(sudoku, sudoku_num, counter) {
@@ -72,7 +73,7 @@ function counter_3(sudoku, sudoku_num, counter) {
     }
   }
   count_three = counter;
-  console.log(`three:${counter}`);
+  //   console.log(`three:${counter}`);
 }
 function counter_4(sudoku, sudoku_num, counter) {
   for (let i = 0; i < 4; i++) {
@@ -81,7 +82,7 @@ function counter_4(sudoku, sudoku_num, counter) {
     }
   }
   count_four = counter;
-  console.log(`four:${counter}`);
+  //   console.log(`four:${counter}`);
 }
 
 function order_of_sudoku_numbers() {
@@ -92,8 +93,6 @@ function order_of_sudoku_numbers() {
   dict["four"] = count_four;
 
   console.log(dict);
-
-  var sortable = [];
   for (var count in dict) {
     sortable.push([count, dict[count]]);
   }
@@ -102,8 +101,40 @@ function order_of_sudoku_numbers() {
     return b[1] - a[1];
   });
   console.log(sortable);
+  //   console.log(sortable[0][0]);
+}
 
-  console.log(sortable[0][0]);
+//TODO: no for loop need to call another section after
+//TODO: [0][0] should be function name
+function can_you_put_a_1_pos0_0() {
+  console.log(first_sudoku);
+  if (
+    first_sudoku[0].indexOf("1") == -1 &&
+    first_sudoku[1][0].indexOf("1") == -1 &&
+    first_sudoku[2][0].indexOf("1") == -1 &&
+    first_sudoku[3][0].indexOf("1") == -1
+  ) {
+    console.log("yes it can put a one here ");
+    // call pos0_1 next
+  } else {
+    console.log("no!!! it has a 1");
+    first_sudoku[0][0] = "N";
+    console.log(first_sudoku);
+    //call pos0_1 next
+  }
+}
+
+function first_number_to_insert() {
+  if (sortable[0][0] == "one") {
+    return can_you_put_a_1_pos0_0();
+    // call function insert_1()
+  } else if (sortable[0][0] == "two") {
+    // call function insert_2()
+  } else if (sortable[0][0] == "three") {
+    // call function insert_3()
+  } else if (sortable[0][0] == "four") {
+    // call function insert_4()
+  }
 }
 
 var one = "1";
@@ -116,3 +147,4 @@ counter_2(first_sudoku, two, count_two);
 counter_3(first_sudoku, three, count_three);
 counter_4(first_sudoku, four, count_four);
 order_of_sudoku_numbers();
+first_number_to_insert();
